2a30a360a84e2cc7d1592d060ca8cec855874270
REFATBHUYAN <refatbubt@gmail.com>
2023-05-18 16:15:00 +0600

    get method added and quary added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
 const app = express();
@@ -28,13 +28,44 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
+
+     const toyCollection = client.db('toyShop').collection('toys');
+    //  
+    app.get('/toys', async(req, res)=>{
+        let query = {};
+        if (req.query?.email) {
+            query = { email: req.query.email }
+        }
+        const result = await toyCollection.find(query).limit(20).toArray();
+        res.send(result);
+    })
+
+    app.get('/toys/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await toyCollection.findOne(query);
+        res.send(result);
+    })
+    // app.get('/toys', async(req, res)=>{
+    //     // const id = req.params.id;
+       
+    //     const result = await toyCollection.find(query).toArray();
+    //     res.send(result);
+    // })
+
+    app.post('/toys', async(req, res)=>{
+        console.log(req.body);
+        const toy = req.body;
+        const result = await toyCollection.insertOne(toy);
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
